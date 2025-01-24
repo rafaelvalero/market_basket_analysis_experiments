7603d040d3e49ea2864aadf8ce1a8278be631220
Rafael Valero Fernández <rafael.valero.fernander@gmail.com>
2025-01-24 20:46:49 +0000

    cleaning fpgrowth example

--- fpgrowth_e1.py
+++ fpgrowth_e1.py
@@ -20,6 +20,7 @@ from pyspark.sql import SparkSession
 from pyspark.ml.fpm import FPGrowth
 import pandas as pd
 import subprocess
+import numpy as np
 
 NAME_NOTEBOOK = 'fpgrowth_e1'
 
@@ -29,119 +30,58 @@ spark = SparkSession.builder.appName("MarketBasketAnalysis").getOrCreate()
 
 # +
 # Sample transaction data
-dataset = [(['Milk', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],),
-           (['Dill', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],),
-           (['Milk', 'Apple', 'Kidney Beans', 'Eggs'],),
-           (['Milk', 'Unicorn', 'Corn', 'Kidney Beans', 'Yogurt'],),
-           (['Corn', 'Onion', 'Kidney Beans', 'Ice cream', 'Eggs'],)]
+dataset = [['Milk', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
+           ['Dill', 'Onion', 'Nutmeg', 'Kidney Beans', 'Eggs', 'Yogurt'],
+           ['Milk', 'Apple', 'Kidney Beans', 'Eggs'],
+           ['Milk', 'Unicorn', 'Corn', 'Kidney Beans', 'Yogurt'],
+           ['Corn', 'Onion', 'Onion', 'Kidney Beans', 'Ice cream', 'Eggs']]
 
 dataset
-
-# +
-# dataset = [['Milk', 'Onion'],
-#            ['Dill']]
-
-# dataset
-
-# +
-# df = spark.createDataFrame(dataset)
-# df.show()
-
-# +
-# from pyspark.sql.types import ArrayType, StringType
-
-# # Sample data with arrays of different lengths
-# data = [
-#     (["apple", "banana", "cherry"],),
-#     (["orange", "grape"],),
-#     (["watermelon"],),
-#     (["pineapple", "mango", "papaya", "kiwi"],)
-# ]
-
-# # Define the schema
-# schema = ArrayType(StringType())
-
-# # Create a DataFrame
-# df = spark.createDataFrame(data, ["fruits"])
-
-# # Show the DataFrame
-# df.show(truncate=False)
 # -
 
-
-
-
-
-# +
-# input_spark_df = list(map(lambda x: (', '.join(x),), dataset))
-# input_spark_df
-
-# +
-# df = spark.createDataFrame(dataset,\
-#                            ["items"])
-# df.show()
-
-# +
-# # Create a Spark DataFrame
-# df = spark.createDataFrame([('milk, onion',),\
-#                             ('milk, onion',)],\
-#                            ["items"])
-# df.show()
-
-# +
-# # Sample data
-# data = [
-#     ("Alice", 25),
-#     ("Bob", 30),
-#     ("Charlie", 35)
-# ]
-
-# # Create a DataFrame
-# df = spark.createDataFrame(data, ["Name", "Age"])
-# df.show()
-
-# +
-# # Sample transaction data
-# data = [
-#     ["Milk", "Onion", "Nutmeg", "Kidney Beans", "Eggs", "Yogurt"],
-#     ["Dill", "Onion", "Nutmeg", "Kidney Beans", "Eggs", "Yogurt"],
-#     ["Milk", "Apple", "Kidney Beans", "Eggs"],
-#     ["Milk", "Unicorn", "Corn", "Kidney Beans", "Yogurt"],
-#     ["Corn", "Onion", "Onion", "Kidney Beans", "Ice cream", "Eggs"],
-# ]
+# Some transfomations, parsing the data.
+# note for fpgrowh basket can only have unique elements.
+dataset = list(map(lambda x: (np.unique(x).tolist(),), dataset))
+dataset
 
 # # Create a Spark DataFrame
 df = spark.createDataFrame(dataset, ["items"])
 
-# -
 
 df.show()
 
 # Apply the FPGrowth algorithm
-fpGrowth = FPGrowth(itemsCol="items", minSupport=0.6, minConfidence=0.6)
+fpGrowth = FPGrowth(itemsCol="items", 
+                    minSupport=0.1, 
+                    minConfidence=0.1)
 model = fpGrowth.fit(df)
 
 
 # Display frequent itemsets
 model.freqItemsets.show()
 
+output = model.freqItemsets
+output.show()
+
+output = output.toPandas()
+output
+
+output.to_csv('freqItemasFPgrowth.csv')
+
 # Display generated association rules
-model.associationRules.show()
+output2 = model.associationRules.toPandas()
+output2
 
 
+output2.to_csv('associaton_rules_FPgrowth.csv')
 
-
-
-
-
-#
-
-
+# Saving
 orden_to_give = f'jupytext --output {NAME_NOTEBOOK}.py {NAME_NOTEBOOK}.ipynb'
-# orden_to_give = f'pwd'
 print(orden_to_give)
 subprocess.call(orden_to_give, shell=True)
 
 # Stop the SparkSession
 spark.stop()
+
+
 
